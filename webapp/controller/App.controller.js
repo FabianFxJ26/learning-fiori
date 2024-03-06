4e73f769b8267e5bea3f0ad9f1f5bb9c37f44c4f
FabianFxJ26 <elkin3001@gmail.com>
2024-03-06 15:00:01 +0000

    First commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("learningfiori.invoices.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
