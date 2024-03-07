3e332cba9f075985abd0c562f846706553fb80d8
FabianFxJ26 <elkin3001@gmail.com>
2024-03-07 21:48:54 +0000

    Sixth commit

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,5 +1,5 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -10,14 +10,14 @@ sap.ui.define([
         //Prototipado, y pasamos un objeto tipo json.
         return Controller.extend("learningfiori.invoices.controller.MainView", {
             onInit: function () {
-                const oJSONModel = new sap.ui.model.json.JSONMODEL();
+                const oJSONModel = new sap.ui.model.json.JSONModel();
                 //Enlazar json model con la vista
                 const oView = this.getView();
                 //Cargo la data definida en el archivo json
-                oJSONModel.loadData("./model/selectionScreenMenu.json");
-                oView.setModel(oJSONModel, "Selection screen");
+                oJSONModel.loadData("../model/SelectionScreenMenu.json");
+                oView.setModel(oJSONModel,"selectionScreen");
                 //Otra forma
-                //this.getView().setModel(oJSONModel,"Selection screen");
+                //this.getView().setModel(oJSONModel,"selectionScreen");
             }
         });
     });
